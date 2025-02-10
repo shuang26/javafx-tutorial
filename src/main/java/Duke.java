e075a4a6d7215a9b8763ec4719e8164cff30a435
shuang26 <shuangyuan9@gmail.com>
2025-02-11 00:47:45 +0800

    Finish step 5 of JavaFx tutorial

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,29 @@
 public class Duke {
+    private String commandType;
+
     public static void main(String[] args) {
         System.out.println("Hello!");
+    }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
+    }
+
+//    public String getResponse(String input) {
+//        try {
+//            Command c = Parser.parse(input);
+//            c.execute(tasks, ui, storage);
+//            commandType = c.getClass().getSimpleName();
+//            return c.getString();
+//        } catch (DukeException e) {
+//            return "Error: " + e.getMessage();
+//        }
+//    }
+
+    public String getCommandType() {
+        return commandType;
     }
 }
